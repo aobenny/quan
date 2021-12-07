fbbbd59a3aba86c6bdb692e78af61a540da7b82a
aobenny <32319501+aobenny@users.noreply.github.com>
2021-12-07 20:47:11 +0800

    Create inface.js

---
+++ VIP/inface.js
@@ -0,0 +1,15 @@
+/*
+[rewrite_local]
+#inface （by bennyao）
+^https:\/\/bmall\.camera360\.com\/api\/inface\/ url script-response-body https://raw.githubusercontent.com/aobenny/quan/main/VIP/inface.js
+[MITM]
+hostname:bmall.camera360.com
+*/
+
+var body = $response.body; 
+var obj = JSON.parse(body); 
+
+obj.items.vip = 0;
+
+body = JSON.stringify(obj); 
+$done(body);
