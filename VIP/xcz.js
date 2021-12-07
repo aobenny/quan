69f2bd91c20c8834e2a66a456118072cfcaea106
aobenny <32319501+aobenny@users.noreply.github.com>
2021-12-07 20:30:16 +0800

    Create xcz.js

---
+++ VIP/xcz.js
@@ -0,0 +1,16 @@
+/*
+[rewrite_local]
+#西窗烛 （by bennyao）
+^https:\/\/lchttpapi\.xczim\.com\/1\.1\/users\/ url script-response-body xcz.js
+[MITM]
+hostname:lchttpapi.xczim.com
+*/
+
+var body = $response.body; 
+var obj = JSON.parse(body); 
+
+obj.lifetimeMembership = true;
+obj.membership = true;
+
+body = JSON.stringify(obj); 
+$done(body);
