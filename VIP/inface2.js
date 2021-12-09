8a6779db1430744057c944f6e67175340895c437
aobenny <32319501+aobenny@users.noreply.github.com>
2021-12-09 12:19:06 +0800

    Update inface2.js

--- VIP/inface2.js
+++ VIP/inface2.js
@@ -1,14 +1,10 @@
 /*
 [rewrite_local]
 #inface （by bennyao）
-^https:\/\/bmall\.camera360\.com\/api\/inface url script-response-body https://raw.githubusercontent.com/aobenny/quan/main/VIP/inface.js
+^https:\/\/bmall\.camera360\.com\/api\/inface\/(stick|effect-art|background|stripes|3dlight|palette|brushes) url script-response-body https://raw.githubusercontent.com/aobenny/quan/main/VIP/inface.js
 [MITM]
 hostname:bmall.camera360.com
 */
-
-var body = $response.body; 
-var obj = JSON.parse(body); 
-
-
-body = JSON.stringify(obj); 
-$done(body);
+var body=$response.body; 
+body=body.replace(/vip\":\d/g,'vip":0');
+$done({body});
