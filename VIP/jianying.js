b36f0f46db6012f70630a27cdb76194d8a33afe3
aobenny <32319501+aobenny@users.noreply.github.com>
2021-12-10 15:23:40 +0800

    Create jianying.js

---
+++ VIP/jianying.js
@@ -0,0 +1,16 @@
+/*
+[rewrite_local]
+#jianying （by bennyao）
+https://commerce-api.faceu.mobi/commerce/v1/purchase/purchase_info?
+https://commerce-api.faceu.mobi/commerce/v1/purchase/user_wallet_info?
+
+^https:\/\/commerce\-api\.faceu\.mobi\/commerce\/v1\/purchase\/* url script-response-body https://raw.githubusercontent.com/aobenny/quan/main/VIP/jingying.js
+[MITM]
+hostname:commerce-api.faceu.mobi
+*/
+
+var body=$response.body; 
+
+body=body.replace(/has_purchased\":false/g,'has_purchased":true').replace(/has_purchased\\\":false/g,'has_purchased\":true');
+
+$done({body});
