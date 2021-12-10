fd235176bb3f098189ddfd13e752167494c4175b
aobenny <32319501+aobenny@users.noreply.github.com>
2021-12-10 16:39:26 +0800

    Update jianying.js

--- VIP/jianying.js
+++ VIP/jianying.js
@@ -7,10 +7,21 @@ https://commerce-api.faceu.mobi/commerce/v1/purchase/user_wallet_info?
 ^https:\/\/commerce\-api\.faceu\.mobi\/commerce\/v1\/purchase\/* url script-response-body https://raw.githubusercontent.com/aobenny/quan/main/VIP/jianying.js
 [MITM]
 hostname:commerce-api.faceu.mobi
-*/
+
 
 var body=$response.body; 
 
 body=body.replace(/has_purchased\":false/g,'has_purchased":true').replace(/has_purchased\\\":false/g,'has_purchased\":true');
 
 $done({body});
+*/
+var body = $response.body; 
+var obj = JSON.parse(body); 
+
+obj.data.total_money=9999;
+obj.data.current_money=9999;
+obj.data.can_buy_template_free=true
+obj.response="{\"user_id\":12848796284315,\"total_money\":0,\"current_money\":0,\"currency_code\":\"CNY\",\"latest_record_time\":0,\"can_buy_template_free\":true}";
+
+body = JSON.stringify(obj); 
+$done(body);
