376b9ab62048f90ebe116f9d4f7e688525617c04
aobenny <32319501+aobenny@users.noreply.github.com>
2021-12-10 15:28:23 +0800

    Update jianying.js

--- VIP/jianying.js
+++ VIP/jianying.js
@@ -4,7 +4,7 @@
 https://commerce-api.faceu.mobi/commerce/v1/purchase/purchase_info?
 https://commerce-api.faceu.mobi/commerce/v1/purchase/user_wallet_info?
 
-^https:\/\/commerce\-api\.faceu\.mobi\/commerce\/v1\/purchase\/* url script-response-body https://raw.githubusercontent.com/aobenny/quan/main/VIP/jingying.js
+^https:\/\/commerce\-api\.faceu\.mobi\/commerce\/v1\/purchase\/* url script-response-body https://raw.githubusercontent.com/aobenny/quan/main/VIP/jianying.js
 [MITM]
 hostname:commerce-api.faceu.mobi
 */
