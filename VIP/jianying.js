639e1116740aebdcdf76a9a7685720605e9f4ce3
aobenny <32319501+aobenny@users.noreply.github.com>
2021-12-10 17:53:49 +0800

    Update jianying.js

--- VIP/jianying.js
+++ VIP/jianying.js
@@ -4,7 +4,7 @@
 https://commerce-api.faceu.mobi/commerce/v1/purchase/purchase_info?
 https://commerce-api.faceu.mobi/commerce/v1/purchase/user_wallet_info?
 
-^https:\/\/commerce\-api\.faceu\.mobi\/commerce\/v1\/purchase\/* url script-response-body https://raw.githubusercontent.com/aobenny/quan/main/VIP/jianying.js
+^https:\/\/commerce-api\.faceu\.mobi\/commerce\/v1\/purchase\/* url script-response-body https://raw.githubusercontent.com/aobenny/quan/main/VIP/jianying.js
 [MITM]
 hostname:commerce-api.faceu.mobi
 
@@ -24,8 +24,8 @@ obj.data.has_purchased=true,
 
 obj.data.total_money=9999;
 obj.data.current_money=9999;
-obj.data.can_buy_template_free=true
-obj.response="{\"user_id\":12848796284315,\"total_money\":9999,\"current_money\":9999,\"currency_code\":\"CNY\",\"latest_record_time\":0,\"can_buy_template_free\":true}";
+obj.data.can_buy_template_free=true;
+obj.response='{\"user_id\":12848796284315,\"total_money\":9999,\"current_money\":9999,\"currency_code\":\"CNY\",\"latest_record_time\":0,\"can_buy_template_free\":true}';
 
 body = JSON.stringify(obj); 
 $done(body);
